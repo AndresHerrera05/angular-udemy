3ef365af9c729500dea48e1f9d4b423c4584c8f9
Andres Herrera <andresherrera@seven4n.com>
2018-03-08 06:56:58 -0500

    Adiciona el componente Header

--- seccion3/hola-mundo/src/app/app.module.ts
+++ seccion3/hola-mundo/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
