83a504456e3de2c1390d883f2e425929b513d216
Andres Herrera <andresherrera@seven4n.com>
2018-04-23 22:23:27 -0500

    Conexión con Firebase, se agrega la petición POST para agregar un héroe a la BD

---
+++ seccion11/heroesapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+import { APP_ROUTING } from './app.routes';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HeroesService } from './services/heroes.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
+  ],
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
